ed1a192cea8493783ed1e09c02ba54f2ef19e001
Linil <linilsunil@gmail.com>
2024-11-21 17:30:45 +0530

    ADDED : CalculatorNode (partial)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import CalculatorNode from "./components/CalculatorNode/CalculatorNode";
 const App: React.FC = () => {
-  return <div className="function-chain"></div>;
+  return (
+    <div className="function-chain">
+      <CalculatorNode />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/CalculatorNode/CalculatorNode.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const CalculatorNode: React.FC = () => {
+  //* Local States - >
+
+  // const [equation, setEquation] = useState<string>("");
+
+  // const handleEquationChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  return (
+    <div className="calculator-node">
+      {/* <input type="text" value={equation} onChange={handleEquationChange} /> */}
+    </div>
+  );
+};
+
+export default CalculatorNode;
