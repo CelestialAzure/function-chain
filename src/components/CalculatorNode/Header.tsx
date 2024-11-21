3b6a9090acf89e0255390ec6b709201ad0b49641
Linil <linilsunil@gmail.com>
2024-11-21 19:17:33 +0530

    ADDED : Header (static ui complete)

--- src/components/CalculatorNode/Header.tsx
+++ src/components/CalculatorNode/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <div className="calculator-node--header">
       <DotCluster />
-      <p className="calculator-node--header--title">Calculator</p>
+      <p className="calculator-node--header--title">Function: 2</p>
     </div>
   );
 };
