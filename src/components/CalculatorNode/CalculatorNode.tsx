798802405cdb400f8c0453d44fc0da0b85dddebd
Linil <linilsunil@gmail.com>
2024-11-22 16:07:32 +0530

    ADDED : body & styling for ExpressionInput comp

--- src/components/CalculatorNode/CalculatorNode.tsx
+++ src/components/CalculatorNode/CalculatorNode.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import ExpressionInput from "./ExpressionInput";
 
 const CalculatorNode: React.FC = () => {
   return (
     <div className="calculator-node">
       <Header />
+      <ExpressionInput />
     </div>
   );
 };
--- src/components/CalculatorNode/ExpressionInput.tsx
+++ src/components/CalculatorNode/ExpressionInput.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "./Header";
 
 const ExpressionInput: React.FC = () => {
   return (
     <div className="expression-input">
-      <Header />
+      <p className="expression-input--header">Equation</p>
+      <input className="expression-input--box" />
     </div>
   );
 };
