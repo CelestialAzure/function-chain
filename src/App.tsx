fc999c5361063c54b09b4573a40319baaa681455
Linil <linilsunil@gmail.com>
2024-11-24 15:55:38 +0530

    ADDED : alignment fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import CalculatorNode from "./components/CalculatorNode/CalculatorNode";
 const App: React.FC = () => {
   return (
-    <div className="function-chain">
-      <CalculatorNode />
+    <div className="function-chain--wrapper">
+      <div className="function-chain">
+        <CalculatorNode />
+        <CalculatorNode />
+        <CalculatorNode />
+        <CalculatorNode />
+        <CalculatorNode />
+      </div>
     </div>
   );
 };
